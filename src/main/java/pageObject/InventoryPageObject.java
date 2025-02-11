d89551722e38e67287e4cc0b82823c2b4da8da89
Nam17kum <Namrata.kumari0605@gmail.com>
2025-02-12 00:02:41 +0530

    commit from develop branch java code

--- src/main/java/pageObject/InventoryPageObject.java
+++ src/main/java/pageObject/InventoryPageObject.java
@@ -3,6 +3,33 @@ package pageObject;
 public class InventoryPageObject {
     public static void main(String[] args) {
         String testData = "Namrata is doing hard to get her job changed";
-        System.out.println(testData);
+//        System.out.println(testData);
+//        System.out.println(reverseString(testData));
+        reverseEachWord(testData);
+    }
+
+    public static String reverseString(String testStr){
+        char[] ch = testStr.toCharArray();
+        String reverseString = "";
+
+        for(int i = ch.length-1; i>=0; i--){
+            reverseString = reverseString + ch[i];
+        }
+        return reverseString;
+    }
+
+    public static void reverseEachWord(String testStr){
+        String[] words = testStr.split("//");
+       //char[] ch = str.toCharArray();
+        String reverseSting = "";
+        String reverseWord = "";
+        for(int i=0; i< words.length; i++){
+            String word = words[i];
+            for(int j =word.length()-1; j>=0;j--) {
+                reverseWord = reverseWord + word.charAt(j);
+            }
+            //reverseSting= reverseWord + reverseSting + "";
+        }
+        System.out.println(reverseWord);
     }
 }
